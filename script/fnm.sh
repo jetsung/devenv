e9d909dd1701256de1b6ff5217ba7ccb65c1424e
Jetsung Chan <jetsungchan@gmail.com>
2026-03-26 13:18:29 +0800

    feat(soft): 添加 FNM Node.js 版本管理器安装脚本

---
+++ script/fnm.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+curl -fsSL fx4.cn/x | bash -s --  https://fnm.vercel.app/install | bash
--- setting/base.sh
+++ setting/base.sh
@@ -26,11 +26,11 @@ fi
 if [[ "$OS" == *"Debian"* ]] || [[ "$OS" == *"Ubuntu"* ]]; then
     # Debian/Ubuntu
     sudo apt-get update
-    sudo apt-get install -y gcc g++ make libc6-dev
+    sudo apt-get install -y gcc g++ make libc6-dev autoconf
     sudo apt-get install -y rpm cpio
 elif [[ "$OS" == *"Fedora"* ]] || [[ "$OS" == *"CentOS"* ]] || [[ "$OS" == *"Red Hat"* ]]; then
     # Fedora/RHEL/CentOS
-    sudo dnf install -y gcc gcc-c++ make glibc-devel
+    sudo dnf install -y gcc gcc-c++ make glibc-devel autoconf
     sudo dnf install -y rpm-build cpio
 else
     echo "Unsupported OS: $OS"
